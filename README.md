205e5c2eaec8c10f687cda829ea6de838612ead6
Charbel K <student@Charbel-K.local>
2015-07-11 10:53:33 +1000

    Removed un-needed code and added help text

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a auto chat script made my me with the help of Arizard! Thanks.
 
 You can use this how you wish
+
+Place script in lua/autorun/client
--- autotext.lua
+++ autotext.lua
@@ -4,8 +4,8 @@ autotext = {
 
 
 
-"Test one two three",
-"test 431231231"
+"Type !motd in chat to view the message of the day!",
+"Press F4 to open pointshop menu"
 
            }
 
@@ -14,12 +14,12 @@ local idx = 1
 
 local function autotextmsg()
 
-local color = Color(255,63,255)
-local color1 = Color(255,43,255)
+local color = Color(255,63,255) -- Colour For ' [HELP] ' Text
+local color1 = Color(255,43,255) -- Colour For Information Text
 
 	chat.AddText(color,"[HELP]",color1,(autotext[idx]))
 	idx = idx + 1
 	if idx > #autotext then idx = 1 end
 end
 
-timer.Create("autotextmsg", 30, 0, autotextmsg )
+timer.Create("autotextmsg", 30, 0, autotextmsg ) -- Repeats every 30 seconds (Cycles through each message)
