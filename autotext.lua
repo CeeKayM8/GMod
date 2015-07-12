9493729cb08bda913536e094d1c7ee1f5c336717
Charbel K <student@Charbel-K.local>
2015-07-12 10:42:07 +1000

    Fixed some spacing that caused an error for some reason

--- autotext.lua
+++ autotext.lua
@@ -14,8 +14,8 @@ local idx = 1
 
 local function autotextmsg()
 
-local color = Color(255,63,255) -- Colour For ' [HELP] ' Text
-local color1 = Color(255,43,255) -- Colour For Information Text
+local color = Color( 255, 63, 255 ) -- Colour For ' [HELP] ' Text
+local color1 = Color( 255, 43, 255 ) -- Colour For Information Text
 
 	chat.AddText(color,"[HELP]",color1,(autotext[idx]))
 	idx = idx + 1
